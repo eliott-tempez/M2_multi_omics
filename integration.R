acbd69090d95934d33d0a9fe974205a00565fdb0
tempezel <tempez.el@gmail.com>
2024-10-17 16:29:22 +0200

    Diablo plots

--- integration.R
+++ integration.R
@@ -53,7 +53,6 @@ plot(explained_var, type = "b", pch = 19, col = "#3d1619",
      xlab = "Nombre de composantes", ylab = "Proportion de Covariance Capturée",
      main = "Proportion de Covariance Capturée - PLS")
 dev.off()
-ggsave(paste0(output_fold, 'pls/elbow_plot.png'))
 # On choisit 5 composantes
 pls <- pls(X = rna_matrix, Y = prot_matrix, ncomp = 5)
 
@@ -71,6 +70,135 @@ plotArrow(pls,
           legend = TRUE, legend.title = "Condition",
           col.per.group = colors, group = condition_group)
 ggsave(paste0(output_fold, 'pls/arrow_plot.png'))
+
+
+
+
+#############################################################################
+#################################   DIABLO   ################################
+#############################################################################
+
+##### Genes and proteins #####
+# set a list of all the X dataframes
+data <-list(RNA = rna_matrix, 
+            protein = prot_matrix)
+Y <- condition_group
+
+# Set design matrix
+design <- matrix(0.1, ncol = length(data), nrow = length(data), 
+                dimnames = list(names(data), names(data)))
+diag(design) <- 0 # set diagonal to 0s
+
+# Form basic DIABLO model
+basic.diablo.model = block.splsda(X = data, Y = Y, ncomp = 5, design = design) 
+
+# run component number tuning with repeated CV
+perf.diablo = perf(basic.diablo.model, validation = 'Mfold', 
+                   folds = 10, nrepeat = 10) 
+
+png(paste0(output_fold, 'diablo/elbow_plot_2.png'))
+plot(perf.diablo, col = colors[1:3]) # plot output of tuning
+dev.off()
+
+# set the optimal ncomp value
+ncomp = perf.diablo$choice.ncomp$WeightedVote["Overall.BER", "centroids.dist"] 
+# show the optimal choice for ncomp for each dist metric
+perf.diablo$choice.ncomp$WeightedVote 
+
+# set grid of values for each component to test
+test.keepX <- list(
+  RNA = c(5, 10, 20, 30, 40, 50),
+  protein = c(5, 10, 20, 30, 40, 50))
+
+# run the feature selection tuning
+tune.TCGA <- tune.block.splsda(X = data, Y = Y, ncomp = ncomp, 
+                              test.keepX = test.keepX, design = design,
+                              validation = 'Mfold', folds = 10, nrepeat = 1,
+                              dist = "centroids.dist")
+list.keepX = tune.TCGA$choice.keepX # set the optimal values of features to retain
+
+# set the optimised DIABLO model
+final.diablo.model = block.splsda(X = data, Y = Y, ncomp = ncomp, 
+                                  keepX = list.keepX, design = design)
+
+# Plot model
+png(paste0(output_fold, 'diablo/plot_2.png'))
+plotDiablo(final.diablo.model, ncomp = 1, col.per.group = colors)
+dev.off()
+
+
+
+##### Genes, proteins and cytokines #####
+# set a list of all the X dataframes
+data <-list(RNA = rna_matrix, 
+            protein = prot_matrix,
+            cyto = cyto_matrix)
+Y <- condition_group
+
+# Set design matrix
+design <- matrix(0.1, ncol = length(data), nrow = length(data), 
+                 dimnames = list(names(data), names(data)))
+diag(design) <- 0 # set diagonal to 0s
+
+# Form basic DIABLO model
+basic.diablo.model = block.splsda(X = data, Y = Y, ncomp = 5, design = design) 
+
+# run component number tuning with repeated CV
+perf.diablo = perf(basic.diablo.model, validation = 'Mfold', 
+                   folds = 10, nrepeat = 10) 
+
+png(paste0(output_fold, 'diablo/elbow_plot_3.png'))
+plot(perf.diablo, col = colors[1:3]) # plot output of tuning
+dev.off()
+
+# set the optimal ncomp value
+ncomp = perf.diablo$choice.ncomp$WeightedVote["Overall.BER", "centroids.dist"] 
+# show the optimal choice for ncomp for each dist metric
+perf.diablo$choice.ncomp$WeightedVote 
+
+# set grid of values for each component to test
+test.keepX <- list(
+  RNA = c(5, 10, 20, 30, 40, 50),
+  protein = c(5, 10, 20, 30, 40, 50),
+  cyto = c(5, 10, 15, 20))
+
+# run the feature selection tuning
+tune.TCGA <- tune.block.splsda(X = data, Y = Y, ncomp = ncomp, 
+                               test.keepX = test.keepX, design = design,
+                               validation = 'Mfold', folds = 10, nrepeat = 1,
+                               dist = "centroids.dist")
+list.keepX = tune.TCGA$choice.keepX # set the optimal values of features to retain
+
+# set the optimised DIABLO model
+final.diablo.model = block.splsda(X = data, Y = Y, ncomp = ncomp, 
+                                  keepX = list.keepX, design = design)
+
+# Plot model
+png(paste0(output_fold, 'diablo/plot_3.png'),
+           width = 800, height = 800, units = "px")
+plotDiablo(final.diablo.model, ncomp = 1, col.per.group = colors)
+dev.off()
+
+
+
+##### Circo and network plot #####
+png(paste0(output_fold, 'diablo/circo_plot.png'),
+    width = 1000, height = 1000, units = "px")
+circosPlot(final.diablo.model, cutoff = 0.7, line = TRUE,
+           color.blocks= colors[1:3],
+           color.cor = c("chocolate3","grey20"), size.labels = 1.5)
+dev.off()
+
+png(paste0(output_fold, 'diablo/network_plot.png'),
+    width = 1000, height = 1000, units = "px")
+network(final.diablo.model, 
+        blocks = c(1,2,3),
+        color.node = colors[2:4], 
+        cutoff = 0.7, 
+        cex.node = 0.4)
+dev.off()
+
+
 
 
 
